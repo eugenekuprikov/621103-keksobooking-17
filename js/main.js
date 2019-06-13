c4757894f40e75776e8e00ea26c149a50974bdb0
eugenekuprikov <cualcua@gmail.com>
2019-06-14 01:06:55 +0300

    Создаем функцию для создания метки на карте

--- js/main.js
+++ js/main.js
@@ -50,3 +50,14 @@ document.querySelector('.map').classList.remove('map--faded');
 var similarListElement = document.querySelector('.map__pins');
 var similarPinTemplate = document.querySelector('#pin')
   .content.querySelector('.map__pin');
+
+var renderPin = function (pin) {
+  var pinElement = similarPinTemplate.cloneNode(true);
+
+  pinElement.style.left = pin.location.x - 25 + 'px';
+  pinElement.style.top = pin.location.y - 70 + 'px';
+  pinElement.querySelector('img').src = pin.author.avatar;
+  pinElement.querySelector('img').alt = 'Заголовок объявления';
+
+  return pinElement;
+};
