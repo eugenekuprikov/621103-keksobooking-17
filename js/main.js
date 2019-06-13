95d1c73978102897253dc8ad31fd4ea1bfeadb87
eugenekuprikov <cualcua@gmail.com>
2019-06-14 00:54:34 +0300

    Создаем функцию для выбора случайных неповторяющихся чисел из данного массива и вызываем ее

--- js/main.js
+++ js/main.js
@@ -2,3 +2,19 @@
 
 var NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8];
 var PLACES = ['palace', 'flat', 'house', 'bungalo'];
+
+var shuffle = function (arr) {
+  var i = arr.length;
+  var j = 0;
+  var temp;
+
+  while (i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  return arr;
+};
+
+var ranNums = shuffle(NUMBERS);
