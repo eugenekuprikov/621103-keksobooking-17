b1f1b5ad515794f91cb02c7b4daa62f3788788be
eugenekuprikov <cualcua@gmail.com>
2019-06-19 20:55:49 +0300

    Добавляем пробел перед запятой

--- js/main.js
+++ js/main.js
@@ -96,7 +96,7 @@ var address = document.querySelector('#address');
 var setValues = function () {
   var x = mapPinMain.getBoundingClientRect().left - 32.5;
   var y = mapPinMain.getBoundingClientRect().top - 87;
-  address.value = [x,y];
+  address.value = [x, y];
 };
 
 setValues();
