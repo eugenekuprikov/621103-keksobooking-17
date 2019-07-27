668d6b8185d85129e44f64658b0a8692e76e7aaa
eugenekuprikov <cualcua@gmail.com>
2019-07-27 16:33:35 +0300

    Создаем функцию с записью данных во вспомогательный файл

---
+++ js/data.js
@@ -0,0 +1,19 @@
+'use strict';
+
+(function () {
+  var NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8];
+  var PLACES = ['palace', 'flat', 'house', 'bungalo'];
+  var YMIN = 130;
+  var YMAX = 630;
+  var XMIN = 0;
+  var XMAX = 1199;
+
+  window.data = {
+    NUMBERS: NUMBERS,
+    PLACES: PLACES,
+    YMIN: YMIN,
+    YMAX: YMAX,
+    XMIN: XMIN,
+    XMAX: XMAX
+  };
+})();
